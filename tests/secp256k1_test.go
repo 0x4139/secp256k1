df0320a453e826916fe22484c046d912d07035dc
0x4139 <0x4139@gmail.com>
2018-04-18 12:27:54 +0300

    adds some tests

---
+++ tests/secp256k1_test.go
@@ -0,0 +1,36 @@
+package tests
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"io"
+	"testing"
+
+	"github.com/0x4139/secp256k1"
+)
+
+func Test_secp256k1(t *testing.T) {
+	secp256k1.Start()
+	defer secp256k1.Stop()
+	var seckey [32]byte
+	io.ReadFull(rand.Reader, seckey[:])
+	if isValid := secp256k1.Seckey_verify(seckey); !isValid {
+		t.FailNow()
+	}
+	pubkey, _ := secp256k1.Pubkey_create(seckey, true)
+	if isValid := secp256k1.Pubkey_verify(pubkey); !isValid {
+		t.FailNow()
+	}
+	msg := make([]byte, 32)
+	io.ReadFull(rand.Reader, msg)
+	hash := sha256.Sum256(msg)
+	var nonce [32]byte
+	io.ReadFull(rand.Reader, nonce[:])
+	sig, ok := secp256k1.Sign(hash, seckey, &nonce)
+	if !ok {
+		t.FailNow()
+	}
+	if ok := secp256k1.Verify(hash, sig, pubkey); !ok {
+		t.FailNow()
+	}
+}
